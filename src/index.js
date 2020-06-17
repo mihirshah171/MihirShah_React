7529736370218d5f1d88d3aafc3e038587bd904a
Mihir Shah <mihir.shah@theonetechnologies.co.in>
2020-06-17 19:48:52 +0530

    Title changed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App/App';
-import * as serviceWorker from './serviceWorker';
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
