0923cfd9ee9c88660538d6a26fbd7844fce64a07
Mihir Shah <mihir.shah@theonetechnologies.co.in>
2020-06-19 13:10:02 +0530

    structure changed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
 import ToDoApp from './components/ToDoApp';
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 import 'bootstrap/dist/css/bootstrap.min.css';
