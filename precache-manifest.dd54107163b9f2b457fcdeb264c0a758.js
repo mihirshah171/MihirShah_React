c2b0c9dd8d01b7edc62d9aaa208f994abd526e78
Mihir Shah <mihir.shah@theonetechnologies.co.in>
2020-06-25 12:47:45 +0530

    Updates

---
+++ precache-manifest.dd54107163b9f2b457fcdeb264c0a758.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bdf67cc6257404b336f294771d4377",
+    "url": "/index.html"
+  },
+  {
+    "revision": "174ebc1dfe62f2cb7297",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7fa66ef1088e5eebdd1a",
+    "url": "/static/css/main.c21ffe0f.chunk.css"
+  },
+  {
+    "revision": "174ebc1dfe62f2cb7297",
+    "url": "/static/js/2.981337dc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.981337dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa66ef1088e5eebdd1a",
+    "url": "/static/js/main.69a0d330.chunk.js"
+  },
+  {
+    "revision": "1ee93e8271d6c8249c9b",
+    "url": "/static/js/runtime-main.bde1966b.js"
+  },
+  {
+    "revision": "3518d52f78a0a93e46435cc6f20142be",
+    "url": "/static/media/brown-paper-clips-1097930.3518d52f.jpg"
+  }
+]);
