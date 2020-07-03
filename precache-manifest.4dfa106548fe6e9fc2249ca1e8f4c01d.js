babd73689568fcba25e716ec585f4138bb625617
Mihir Shah <mihir.shah@theonetechnologies.co.in>
2020-07-03 11:33:04 +0530

    Updates

---
+++ precache-manifest.4dfa106548fe6e9fc2249ca1e8f4c01d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63fa6b96f96732f5fbefcd814c66f4c8",
+    "url": "/MihirShah_React/index.html"
+  },
+  {
+    "revision": "475d5bdefdb9e17a4611",
+    "url": "/MihirShah_React/static/css/2.aa628720.chunk.css"
+  },
+  {
+    "revision": "19f7784ef02496856592",
+    "url": "/MihirShah_React/static/css/main.3828b836.chunk.css"
+  },
+  {
+    "revision": "475d5bdefdb9e17a4611",
+    "url": "/MihirShah_React/static/js/2.e8062bce.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MihirShah_React/static/js/2.e8062bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19f7784ef02496856592",
+    "url": "/MihirShah_React/static/js/main.f76168a2.chunk.js"
+  },
+  {
+    "revision": "a81547fe0fff1b97f1c6",
+    "url": "/MihirShah_React/static/js/runtime-main.d699eab3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MihirShah_React/precache-manifest.6bdd909e31b108e3529765c34dacc914.js"
+  "/MihirShah_React/precache-manifest.4dfa106548fe6e9fc2249ca1e8f4c01d.js"
 );
 
 self.addEventListener('message', (event) => {
