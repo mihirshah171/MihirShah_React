f9968a6375a309dbcf15e3c6622c2846b13b644c
Mihir Shah <mihir.shah@theonetechnologies.co.in>
2020-07-07 10:57:13 +0530

    API Call With Axios

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,46 @@
+import React, { Component, Fragment } from 'react';
+import Modals from '../../components/UI/Modals/Modals';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component {
+        state = {
+            error: null
+        }
+
+        componentDidMount() {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
+                this.setState({ error: null });
+                return req;
+            });
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
+                this.setState({ error: error });
+            });
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({ error: null });
+        }
+
+        render() {
+            return (
+                <Fragment>
+                    <Modals
+                        show={this.state.error}
+                        onHide={this.errorConfirmedHandler}
+                        header='Error'
+                    >
+                        {this.state.error ? this.state.error.message : null}
+                    </Modals>
+                    <WrappedComponent {...this.props} />
+                </Fragment>
+            );
+        }
+    }
+}
+
+export default withErrorHandler;
